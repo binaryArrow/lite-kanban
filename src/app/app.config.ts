5abcab91ea2cacbd95924b1367424a87500402d8
binaryArrow <cancihan96@gmail.com>
2026-09-07 18:56:51 +0200

    chore: deps update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,10 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
